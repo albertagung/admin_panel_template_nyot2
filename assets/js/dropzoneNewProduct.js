08e12f7f06ae519207114f69d9413cf33ab1d57c
albertagung <albiardtya@gmail.com>
2018-04-06 17:45:56 +0700

    adding processQueue manually

--- assets/js/dropzoneNewProduct.js
+++ assets/js/dropzoneNewProduct.js
@@ -1,25 +1,27 @@
 // Cannot inside document ready because options need to go at the beginning
 
 // Generate random id
-	generateRandomId = () => {
-		let randomId = ''
-		let possible = "abcdefghijklmnopqrstuvwxyz1234567890"
-		for (let i = 0; i < 5; i++ ) {
-			randomId += possible.charAt(Math.floor(Math.random() * possible.length))
-		}
-		return randomId
+generateRandomId = () => {
+	let randomId = ''
+	let possible = "abcdefghijklmnopqrstuvwxyz1234567890"
+	for (let i = 0; i < 5; i++ ) {
+		randomId += possible.charAt(Math.floor(Math.random() * possible.length))
 	}
+	return randomId
+}
 
 // multiple file upload
 Dropzone.options.mDropzoneTwo = {
   paramName: "image", // The name that will be used to transfer the file
   maxFiles: 5,
-  maxFilesize: 10, // MB
+  maxFilesize: 1, // 1 MB
   addRemoveLinks: true,
  	uploadMultiple: false,
  	headers: {'fileName': 'apa'},
  	headers: {'fileSection': 'product-11111'},
- 	parallelUploads: 1,
+ 	parallelUploads: 5,
+ 	createImageThumbnails: true,
+ 	autoProcessQueue: false,
  	// Change the file name
  	renameFile: (file) => {
  		return file.name = `${new Date()}-${generateRandomId()}`
@@ -33,9 +35,12 @@ Dropzone.options.mDropzoneTwo = {
   	return imageData
   },
   init: () => {
-  	console.log('apa')
+  	// Add event listener on submit button to process queue manually
+  	let btnSubmit = $('#btnSubmit')
+  	mDropzoneTwo = Dropzone.forElement('.dropzone')
+  	btnSubmit.click((e) => {
+  		e.preventDefault()
+  		mDropzoneTwo.processQueue()
+  	})
   }
-
-  // TODO: coba upload file yang sama error atau ngga
-  // TODO: kalau error bikin nama file random / validasi sebelum ngepost
 };
